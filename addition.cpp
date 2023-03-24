553844ff82ef7d5f8f0c493acfd5c3f29626c0d3
Yash Munde <yashmunde68@gmail.com>
2023-03-25 01:58:45 +0530

    initial commit

---
+++ addition.cpp
@@ -0,0 +1,9 @@
+// this is sample for the addition of two numbers to be used in the git tutorials 
+#include<iostream>
+using namespace std ; 
+int main (){
+    int a=4 , b= 5; 
+    cout<<(a+b)<<endl;
+    cout<<"Addition of two numbers is printed \n";
+    return 0 ;
+}
---
+++ sum.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5, b = 100;
+    int sum = (a + b);
+    cout << "Sum is :" << sum << endl;
+    return 0;
+}
