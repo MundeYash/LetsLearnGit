8b03f2d6179d3394b23ec179ec5872475c4c06e7
Yash Munde <yashmunde68@gmail.com>
2023-03-25 02:32:53 +0530

    adding multipy.cpp file to the my repository

---
+++ multiply.cpp
@@ -0,0 +1,8 @@
+// this is the multiply file for the adding it inside to munde/pariwar branch without informing the other branches
+#include<iostream>
+using namespace std ; 
+int main(){
+    int a=5 , b = 10 ; 
+    cout<<"The multiply value is :"<<(a*b)<<endl;
+    return  0 ; 
+}
